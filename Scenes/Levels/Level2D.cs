78abc57b0a698e558729eb09ba53e4e77ed9b993
Maaack <marek.belski@gmail.com>
2024-03-01 13:54:36 -0800

    Proper warping at edge of levels.

--- Scenes/Levels/Level2D.cs
+++ Scenes/Levels/Level2D.cs
@@ -6,10 +6,52 @@ public partial class Level2D : Node2D
 	[Signal]
 	public delegate void LevelWonEventHandler();
 	[Signal]
-	public delegate void LevelLostEventHandler(); 
+	public delegate void LevelLostEventHandler();
+	[Export]
+	public Vector2 WorldSize = new Vector2(640, 360);
+	[Export]
+	public float WorldSizeMod = 1.0f;
 	protected Node2D Player;
 	public override void _Ready()
 	{
 		Player = GetNode<Node2D>("%Player2D");
+	}
+
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+		var childNodes = GetChildren();
+		var fullWorldSize = WorldSizeMod * WorldSize;
+		foreach ( Node2D child in childNodes )
+		{
+			if ( child is RigidBody2D rigidChild )
+			{
+				var translatedVector = rigidChild.Position;
+				if ( rigidChild.Position.X < - fullWorldSize.X / 2 )
+				{
+					translatedVector.X = fullWorldSize.X / 2 + (rigidChild.Position.X + fullWorldSize.X / 2);
+				} 
+				else if ( rigidChild.Position.X > fullWorldSize.X / 2 )
+				{
+					translatedVector.X = -fullWorldSize.X / 2 + (rigidChild.Position.X - fullWorldSize.X / 2);
+				}
+				if ( rigidChild.Position.Y < - fullWorldSize.Y / 2  )
+				{
+					translatedVector.Y = fullWorldSize.Y / 2 + (rigidChild.Position.Y + fullWorldSize.Y / 2);
+				} 
+				else if ( rigidChild.Position.Y > fullWorldSize.Y / 2 )
+				{
+					translatedVector.Y = -fullWorldSize.Y / 2 + (rigidChild.Position.Y - fullWorldSize.Y / 2);
+				}
+				if (!(translatedVector - rigidChild.Position).IsZeroApprox())
+				{
+					PhysicsServer2D.BodySetState(
+						rigidChild.GetRid(),
+						PhysicsServer2D.BodyState.Transform,
+						new Transform2D(rigidChild.Rotation, translatedVector)
+					);
+				}
+			}
+		}
 	}
 }
